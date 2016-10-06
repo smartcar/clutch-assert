72862efdb8c9a5fcb0c9f688bc6e67281ba89541
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-10-06 15:49:58 -0700

    enhance the assertion library with empower

---
+++ index.js
@@ -0,0 +1,25 @@
+const empower = require('empower');
+const formatter = require('power-assert-formatter');
+
+const assert = require('./lib/assert');
+
+const empowerOptions = {
+  modifyMessageOnRethrow: true,
+  saveContextOnRethrow: true,
+};
+
+function customize(customOptions) {
+  const options = customOptions || {};
+  const poweredAssert = empower(
+    assert,
+    formatter(options.output),
+    Object.assign(empowerOptions, options.assertion)
+  );
+  poweredAssert.customize = customize;
+  return poweredAssert;
+}
+
+const clutch = customize();
+
+clutch.default = clutch;
+module.exports = clutch;
